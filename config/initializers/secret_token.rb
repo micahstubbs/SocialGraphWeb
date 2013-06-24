2f4ec06d86dfab2c5ff8e02ec83d1db08fe44ea8
micahstubbs <micah.stubbs@gmail.com>
2013-06-24 10:54:19 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SocialGraph::Application.config.secret_token = '1ff603d046cbbf22120f3f849627d63cb368611e387dc4131ecb32752a051d98cf5046e8557084253c50d35de725f566cb970bfde2976fe64cd218c59acc3274'
